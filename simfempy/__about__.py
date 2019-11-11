1d8767f1ce36dc5698ad3b82d353a36c21e73ed6
Roland Becker <roland.becker@univ-pau.fr>
2019-11-11 17:54:45 +0100

    xxx

--- simfempy/__about__.py
+++ simfempy/__about__.py
@@ -2,10 +2,9 @@
 # -*- coding: utf-8 -*-
 #
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __author__ = "Roland Becker"
 __author_email__ = "beckerrolandh@gmail.com"
 __copyright__ = u"Copyright (c) 2013-2018, {} <{}>".format(__author__, __author_email__)
 __website__ = "https://github.com/beckerrh/simfempy"
 __license__ = "License :: OSI Approved :: MIT License"
-__status__ = "Development"
