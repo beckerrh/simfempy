c8b3dd9a801b42b0631f03a73fcf534fc2a44f1f
Roland Becker <roland.becker@univ-pau.fr>
2020-04-10 16:35:06 +0200

    analy sol

--- simfempy/tools/analyticalsolution.py
+++ simfempy/tools/analyticalsolution.py
@@ -24,21 +24,27 @@ class AnalyticalSolution():
             expr = expr.replace('z', 'x2')
         if dim==1 and expr.find('x0') == -1:
             expr = expr.replace('x', 'x0')
-        print("expr",expr)
+        # print("expr",expr)
         self.dim, self.expr = dim, expr
         symbc = ""
         for i in range(dim):
             symbc += f"x{i},"
         symbc = symbc[:-1]
-        print("symbc", symbc, "self", self)
+        # print("symbc", symbc, "self", self)
         s = sympy.symbols(symbc)
-        print("s", s)
+        # print("s", s)
         self.fct = np.vectorize(sympy.lambdify(symbc,expr))
         self.fct_x = []
+        self.fct_xx = []
         for i in range(dim):
             if dim==1: fx = sympy.diff(expr, s)
             else: fx = sympy.diff(expr, s[i])
             self.fct_x.append(np.vectorize(sympy.lambdify(symbc, fx),otypes=[float]))
+            self.fct_xx.append([])
+            for j in range(dim):
+                if dim == 1: fxx = sympy.diff(fx, s)
+                else: fxx = sympy.diff(fx, s[j])
+                self.fct_xx[i].append(np.vectorize(sympy.lambdify(symbc, fxx),otypes=[float]))
     def d(self, i, x):
         return self.fct_x[i](*x)
     def x(self, x):
@@ -47,6 +53,26 @@ class AnalyticalSolution():
         return self.fct_x[1](*x)
     def z(self, x):
         return self.fct_x[2](*x)
+    def dd(self, i, j, x):
+        return self.fct_xx[i][j](*x)
+    def xx(self, x):
+        return self.fct_xx[0][0](*x)
+    def xy(self, x):
+        return self.fct_xx[0][1](*x)
+    def xz(self, x):
+        return self.fct_xx[0][2](*x)
+    def yy(self, x):
+        return self.fct_xx[1][1](*x)
+    def yx(self, x):
+        return self.fct_xx[1][0](*x)
+    def yz(self, x):
+        return self.fct_xx[1][2](*x)
+    def zz(self, x):
+        return self.fct_xx[2][2](*x)
+    def zx(self, x):
+        return self.fct_xx[2][0](*x)
+    def zy(self, x):
+        return self.fct_xx[2][1](*x)
 
 
 #=================================================================#
@@ -176,33 +202,19 @@ def analyticalSolution(function, dim, ncomp=1, random=True):
 # ------------------------------------------------------------------- #
 if __name__ == '__main__':
     def test1D():
-        u = AnalyticalSolution3d('x*x')
-        x = np.linspace(0,2,3)
-        print("u.xx(3)", u.xx(3))
-        print("u.x(x)", x, u.x(x))
-        print("u(x)", x, u(x))
-    def test3D():
-        u = AnalyticalSolution3d('x*x+2*y*y+ 4*z*z')
-        for (x,y,z) in [(0,0,0), (1,1,1), (1,0,1), (0,1,0)]:
-            print('Quadratic function', u, ' in x,y,z=:',x,y,z, ' equals: ', u(x,y,z))
-            print('grad=', u.x(x,y,z), u.y(x,y,z), u.z(x,y,z))
-            print('laplace=', u.xx(x,y,z) + u.yy(x,y,z) + u.zz(x,y,z))
+        u = AnalyticalSolution(dim=1, expr='x*x')
+        print("u(2)", u([2]))
+        x = np.meshgrid(np.linspace(0, 2, 3))
+        print("x", x, "\nu=", u.expr, "\nu(x)", u(x), "\nu.x(x)", u.x(x), "\nu.xx(x)", u.xx(x))
+    def test2D():
+        u = AnalyticalSolution(dim=2, expr='x*x*y + y*y')
+        print("u(2,1)", u([2,0]))
+        x = np.meshgrid(np.linspace(0, 2, 3),np.linspace(0, 1, 2))
+        print("x", x, "\nu=", u.expr, "\nu(x)", u(x), "\nu.x(x)", u.x(x), "\nu.xx(x)", u.xx(x))
 
-    # test2D()
-    # test3D()
-    # u = AnalyticalSolution(dim=2, expr='x*y')
-    # print("u(1,2)", u((1,2)))
-    # print("Du(1,2)", u.x((1,2)), u.y((1,2)))
-    # x = np.linspace(0, 2, 3)
-    # y = np.linspace(1, 2, 2)
-    # coord = np.meshgrid(x,y)
-    # print("u(coord)", u.x(coord))
-    u = AnalyticalSolution(dim=1, expr='x*x')
-    print("u(2)", u([2]) )
-    x = np.meshgrid(np.linspace(0, 2, 3))
-    print("x", x, "u(x)", u(x), "u.x(x)", u.x(x))
-    # print("u.x(x)", x, u.x(x))
-    # print("u.xx(x)", x, u.xx(x))
+
+    test2D()
+    # test1D()
 
     
     
